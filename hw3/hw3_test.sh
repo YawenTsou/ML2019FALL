f904e7f932abfe456cb42ed609439a1e4c60903d
YawenTsou <55736661+YawenTsou@users.noreply.github.com>
2019-11-05 19:44:16 +0800

    Add files via upload

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/7p7xm91lttchum9/model_70.pth?dl=1 -O 'model_70.pth'
+wget https://www.dropbox.com/s/n6oclv71dx28g6f/model_91.pth?dl=1 -O 'model_70.pth'
 python hw3_testing.py $1 $2
