032d1bf37f5f33e810ee4242c276c9379eb5606a
YawenTsou <55736661+YawenTsou@users.noreply.github.com>
2019-12-13 20:27:54 +0800

    Add files via upload

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/x7o6p5htl3v9ifr/LSTM%289%29.pth?dl=1 -O 'LSTM(9).pth'
+wget https://www.dropbox.com/s/x700wklw8bti5i3/model_6_1213.pth?dl=1 -O 'model_6_1213.pth'
 python hw5_testing.py $1 $2
